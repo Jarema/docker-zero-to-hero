d141ef7621d35f2eb066b50aee10e83ecda15470
Tomasz Pietrek <tomasz.pietrek@sap.com>
2016-10-17 13:03:20 +0200

    fix bridge example

--- network/bridge.sh
+++ network/bridge.sh
@@ -2,4 +2,4 @@
 
 # run on bridge
 docker run --name webapp -d training/webapp
-docker run --link webapp --name -p 8080:80 nginx -v `pwd`/nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
+docker run --link webapp --name nginx -p 8080:80 -v `pwd`/nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
