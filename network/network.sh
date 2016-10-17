85555b4bafa0714bd25faa6194fa821efdb2ec75
Tomasz Pietrek <tomasz.pietrek@sap.com>
2016-10-17 10:33:57 +0200

    fix permissions

--- network/network.sh
+++ network/network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # run on user defined network
-docker network create --name demo
+docker network create --driver bridge demo
 docker run -d --name webapp --net=demo training/webapp
-docker run -d --name nginx -l 8080:80 --net=demo -v `pwd`/nginx.conf:/etc/nginx/nginx.conf:ro nginx
+docker run -d --name nginx -p 8080:80 --net=demo -v `pwd`/nginx.conf:/etc/nginx/nginx.conf:ro nginx
